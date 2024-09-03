cbdc7800a77060757f99d4a6a09afdd658590018
Roland Szabó <hi@rolandszabo.design>
2024-09-03 15:40:21 +0200

    v2.0
    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,6 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/assets");
 	eleventyConfig.addWatchTarget("src/assets/css");
 	eleventyConfig.setQuietMode(true);
-	eleventyConfig.setLiquidOptions({
-    	dynamicPartials: false,
-    });
 
 	return {
 		dir: {
